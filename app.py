a239007fd71fef300e9bbbeca23c1a8465266b6a
yashwanthhassan2-web <yashwanthhassan2@gmail.com>
2026-03-27 20:26:34 +0530

    requirements.txt

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Algo running 🚀"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print(data)
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run()
